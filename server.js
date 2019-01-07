b2b952fa5664e347a346efc091b3952004fe61d7
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-01-07 11:19:01 -0800

    cannot serve the file up with express server due to a babel weird error so must fix it later cause app runs slow without express

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 const App = require("./src/App");
+// require("babel-core").transform(App, {
+//   presets: ["react"]
+// });
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
@@ -13,7 +16,7 @@ app.get("/ping", function(req, res) {
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, App, "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, App, "index.html"));
+});
 app.listen(port);
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+// const React = require("react");
 import "./App.css";
 import { Button } from "react-bootstrap";
 
