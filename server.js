64509d57c50dfd7e25ca9610e9dc261d593f4579
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-01-14 17:52:43 -0800

    init

--- server.js
+++ server.js
@@ -16,14 +16,8 @@ app.get("/ping", function(req, res) {
 // app.get("/", function(req, res) {
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
 app.get("*", function(req, res) {
   const index = path.join(__dirname, "build", "index.html");
   res.sendFile(index);
 });
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "index.html"));
-// });
 app.listen(port);
