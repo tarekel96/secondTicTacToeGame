5b9d32fe7fa04f393ebd2e4062f6250e6fbd189b
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-01-14 17:50:19 -0800

    figured out how to serve this application express to make it start faster next going to update the heroku version

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
-const App = require("./src/App");
+// const App = require("./src/App");
 // require("babel-core").transform(App, {
 //   presets: ["react"]
 // });
@@ -13,10 +13,17 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("/ping", function(req, res) {
   return res.send("pong");
 });
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+app.get("*", function(req, res) {
+  const index = path.join(__dirname, "build", "index.html");
+  res.sendFile(index);
 });
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, App, "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "index.html"));
+// });
 app.listen(port);
