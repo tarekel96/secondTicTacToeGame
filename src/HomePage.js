44404cc9d2564ad68aab483368f8b8a3f82b7748
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-02-08 15:38:40 -0800

    fixed the background image issue with static file

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import "./HomePage.css";
 import { Button } from "react-bootstrap";
+import "./HomePage.css";
 
 class HomePage extends Component {
   // state = {};
